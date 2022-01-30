90b885049bdfca8ba7f689ea25623781cbaefefe
Kim Chae Eun <kce3615@naver.com>
2022-01-31 01:00:22 +0900

    slide animation

--- src/App.tsx
+++ src/App.tsx
@@ -16,40 +16,60 @@ const Box = styled(motion.div)`
   height: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  position: absolute;
   top: 50px;
+  font-size: 28px;
+  justify-content: center;
+  align-items: center;
+  display: flex;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  position: absolute;
 `;
 const boxVarients = {
-  initial: {
+  initial: (back: boolean) => ({
     opacity: 0,
     scale: 0,
-  },
+    x: back ? -500 : 500,
+  }),
   visible: {
     opacity: 1,
     scale: 1,
-    rotateZ: 360,
+    x: 0,
+    transition: { duration: 1 },
   },
-  leaving: {
+  leaving: (back: boolean) => ({
     opacity: 0,
-    y: 50,
-  },
+    scale: 0,
+    x: back ? 500 : -500,
+    transition: { duration: 1 },
+  }),
 };
 function App() {
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState(1);
+  const [back, setBack] = useState(false);
+  const prevClick = () => {
+    setBack(() => true);
+    setShow((prev) => (prev === 1 ? 1 : prev - 1));
+  };
+  const nextClick = () => {
+    setBack(() => false);
+    setShow((prev) => (prev === 10 ? 10 : prev + 1));
+  };
   return (
     <Wrapper>
       <AnimatePresence>
-        {show ? (
-          <Box
-            variants={boxVarients}
-            initial="initial"
-            animate="visible"
-            exit="leaving"
-          />
-        ) : null}
+        <Box
+          key={show}
+          variants={boxVarients}
+          initial="initial"
+          animate="visible"
+          exit="leaving"
+          custom={back}
+        >
+          {show}
+        </Box>
       </AnimatePresence>
-      <button onClick={() => setShow((prev) => !prev)}>toggle</button>
+      <button onClick={prevClick}>prev</button>
+      <button onClick={nextClick}>next</button>
     </Wrapper>
   );
 }
