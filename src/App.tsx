ffc99369971e9fdaf11d0c3b72d69ad65db161af
Kim Chae Eun <kce3615@naver.com>
2022-02-01 22:57:49 +0900

    layout basic

--- src/App.tsx
+++ src/App.tsx
@@ -3,73 +3,42 @@ import { AnimatePresence, motion } from "framer-motion";
 import styled from "styled-components";
 
 const Wrapper = styled(motion.div)`
-  display: flex;
-  width: 100vw;
   height: 100vh;
-  justify-content: center;
+  width: 100vw;
+  display: flex;
+  justify-content: space-around;
   align-items: center;
   background: linear-gradient(135deg, #e09, #d0e);
 `;
 
 const Box = styled(motion.div)`
-  width: 400px;
-  height: 200px;
+  width: 300px;
+  height: 300px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  top: 50px;
   font-size: 28px;
+  display: flex;
   justify-content: center;
   align-items: center;
-  display: flex;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  position: absolute;
 `;
-const boxVarients = {
-  initial: (back: boolean) => ({
-    opacity: 0,
-    scale: 0,
-    x: back ? -500 : 500,
-  }),
-  visible: {
-    opacity: 1,
-    scale: 1,
-    x: 0,
-    transition: { duration: 1 },
-  },
-  leaving: (back: boolean) => ({
-    opacity: 0,
-    scale: 0,
-    x: back ? 500 : -500,
-    transition: { duration: 1 },
-  }),
-};
+const Circle = styled(motion.div)`
+  width: 100px;
+  height: 100px;
+  border-radius: 100px;
+  background-color: #9494e6;
+`;
 function App() {
-  const [show, setShow] = useState(1);
-  const [back, setBack] = useState(false);
-  const prevClick = () => {
-    setBack(() => true);
-    setShow((prev) => (prev === 1 ? 1 : prev - 1));
-  };
-  const nextClick = () => {
-    setBack(() => false);
-    setShow((prev) => (prev === 10 ? 10 : prev + 1));
+  const [clicked, setClicked] = useState(false);
+  const onClick = () => {
+    setClicked((prev) => !prev);
   };
   return (
-    <Wrapper>
-      <AnimatePresence>
-        <Box
-          key={show}
-          variants={boxVarients}
-          initial="initial"
-          animate="visible"
-          exit="leaving"
-          custom={back}
-        >
-          {show}
-        </Box>
-      </AnimatePresence>
-      <button onClick={prevClick}>prev</button>
-      <button onClick={nextClick}>next</button>
+    <Wrapper onClick={onClick}>
+      <Box>{clicked ? <Circle layoutId="circle" /> : null}</Box>
+      <Box>
+        {!clicked ? <Circle style={{ scale: "2" }} layoutId="circle" /> : null}
+      </Box>
     </Wrapper>
   );
 }
