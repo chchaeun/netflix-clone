7dbee6fc1d6c44930899c6ff63c211a910665839
Kim Chae Eun <kce3615@naver.com>
2022-02-03 22:31:00 +0900

    animation challenge

--- src/App.tsx
+++ src/App.tsx
@@ -12,21 +12,27 @@ const Wrapper = styled(motion.div)`
 `;
 const Grid = styled(motion.div)`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(2, 1fr);
   width: 50vw;
   gap: 10px;
-  div:first-child,
-  div:last-child {
-    grid-column: span 2;
-  }
 `;
 
 const Box = styled(motion.div)`
-  height: 200px;
-  background-color: rgba(255, 255, 255, 1);
+  height: 150px;
+  background-color: rgba(255, 255, 255, 0.5);
   border-radius: 40px;
   display: flex;
+  justify-content: center;
+  align-items: center;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Circle = styled(motion.div)`
+  width: 50px;
+  height: 50px;
+  border-radius: 100px;
+  background-color: white;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.1);
 `;
 const Overlay = styled(motion.div)`
   width: 100%;
@@ -36,19 +42,52 @@ const Overlay = styled(motion.div)`
   align-items: center;
   display: flex;
 `;
+const OverlayBox = styled(Box)`
+  width: 400px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 1);
+`;
 const overlay = {
   visible: { backgroundColor: "rgba(0, 0, 0, 0.3)" },
   invisible: { backgroundColor: "rgba(0, 0, 0, 0)" },
 };
+const Button = styled.button`
+  position: absolute;
+  width: 50px;
+  height: 25px;
+  bottom: 5%;
+  border: none;
+  border-radius: 4px;
+  background-color: white;
+  cursor: pointer;
+`;
 function App() {
   const [id, setId] = useState<null | number>(null);
+  const [click, setClick] = useState<boolean>(false);
   return (
     <Wrapper>
       <Grid>
-        {[1, 2, 3, 4].map((n) => (
-          <Box onClick={() => setId(n)} key={n} layoutId={n + ""} />
+        {[
+          [-1, -1],
+          [1, -1],
+          [-1, 1],
+          [1, 1],
+        ].map((v, n) => (
+          <Box
+            whileHover={{ scale: 1.2, x: `${v[0] * 10}%`, y: `${v[1] * 10}%` }}
+            onClick={() => setId(n + 1)}
+            key={n + 1}
+            layoutId={n + 1 + ""}
+          >
+            <AnimatePresence>
+              {(n === 1 && click) || (n === 2 && !click) ? (
+                <Circle layoutId="circle" />
+              ) : null}
+            </AnimatePresence>
+          </Box>
         ))}
       </Grid>
+      <Button onClick={() => setClick((prev) => !prev)}>Switch</Button>
       <AnimatePresence>
         {id ? (
           <Overlay
@@ -58,7 +97,7 @@ function App() {
             animate="visible"
             exit="invisible"
           >
-            <Box layoutId={id + ""} style={{ width: "400px" }} />
+            <OverlayBox layoutId={id + ""} />
           </Overlay>
         ) : null}
       </AnimatePresence>
